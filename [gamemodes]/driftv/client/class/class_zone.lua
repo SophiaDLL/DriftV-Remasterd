d6c60062618192bbfe8eafd12ddd401915e1cd4f
Rubylium <19718604+Rubylium@users.noreply.github.com>
2024-09-05 15:56:34 +0200

    refactor(driftv): Refactor class_zone.lua for improved readability and performance

--- [gamemodes]/driftv/client/class/class_zone.lua
+++ [gamemodes]/driftv/client/class/class_zone.lua
@@ -1,92 +1,74 @@
--- "simulation" d'une class car Lua n'a pas vraiment de class, c'est juste pratique de faire ça
-zone = {
-    zones = {},
-    
-    addZone = function(name, pos, text, action_, haveMarker, markerType, markerSize, markerColor, markerAlpha, dict, marker, rotX, rotY, rotZ)
-        print("[info]: Adding new zone "..name)
+zone = {}
+zone.zones = {}
+zone.addZone = function(name, pos, text, action_, haveMarker, markerType, markerSize, markerColor, markerAlpha, dict, marker, rotX, rotY, rotZ)
+    print("[info]: Adding new zone " .. name)
 
+    -- Set default values for rotation if not provided
+    rotX = rotX or 0.0
+    rotY = rotY or 0.0
+    rotZ = rotZ or 0.0
 
-        if rotX == nil then
-            rotX = 0.0
-        end
+    local newZone = {
+        name = name,
+        pos = pos,
+        text = text,
+        actions = action_,
+        haveMarker = haveMarker,
+        markerType = markerType,
+        markerSize = markerSize,
+        markerColor = markerColor,
+        markerAlpha = markerAlpha,
+        dict = dict,
+        marker = marker,
+        rotX = rotX,
+        rotY = rotY,
+        rotZ = rotZ,
+    }
+    zone.zones[name] = newZone
+end
 
-        if rotY == nil then
-            rotY = 0.0
-        end
-
-        if rotZ == nil then
-            rotZ = 0.0
-        end
-
-        local newZone = {
-            name = name,
-            pos = pos,
-            text = text,
-            actions = action_,
-            haveMarker = haveMarker,
-            markerType = markerType,
-            markerSize = markerSize,
-            markerColor = markerColor,
-            markerAlpha = markerAlpha,
-            dict = dict, 
-            marker = marker,
-            rotX = rotX,
-            rotY = rotY,
-            rotZ = rotZ,
-        }
-        zone.zones[name] = newZone
-    end,
-
-    removeZone = function(name)
-        zone.zones[name] = nil
-    end,
-}
-
+zone.removeZone = function(name)
+    zone.zones[name] = nil
+end
 
 -- @ Zone handler
+
+local closestZones = {}
+local lastUpdateTime = 0
+local updateInterval = 250 -- milliseconds
 
 Citizen.CreateThread(function()
     while p == nil do Wait(1000) end
     while true do
-        local pNear = false
-
-        for k,v in pairs(zone.zones) do
-            local dst = GetDistanceBetweenCoords(p:pos(), v.pos, true)
-
-            if dst <= 20.0 and not p:GetCrewWarStatus() then
-                pNear = true
-                if v.haveMarker then
-
-                    -- if v.dict ~= nil then
-                    --     if not HasStreamedTextureDictLoaded(v.dict) then
-                    --         RequestStreamedTextureDict(v.dict, true)
-                    --         while not HasStreamedTextureDictLoaded(v.dict) do
-                    --             print("Loading custom marker ... ", v.dict, v.marker)
-                    --             Wait(1)
-                    --         end
-                    --         SetStreamedTextureDictAsNoLongerNeeded(v.dict)
-                    --     end
-
-                    --     DrawMarker(v.markerType, v.pos, 0.0, 0.0, 0.0, v.rotX, v.rotY, v.rotZ, v.markerSize, v.markerSize, v.markerSize, v.markerColor[1], v.markerColor[2], v.markerColor[3], v.markerAlpha, 0, 1, 2, 0, v.dict, v.marker, 0)
-                    -- else
-                        DrawMarker(v.markerType, v.pos, 0.0, 0.0, 0.0, v.rotX, v.rotY, v.rotZ, v.markerSize, v.markerSize, v.markerSize, v.markerColor[1], v.markerColor[2], v.markerColor[3], v.markerAlpha, 0, 1, 2, 0, nil, nil, 0)
-
-                    -- end
+        local currentTime = GetGameTimer()
+        
+        -- Refresh distances every 250 ms
+        if currentTime - lastUpdateTime >= updateInterval then
+            closestZones = {}
+            for k, v in pairs(zone.zones) do
+                local dst = GetDistanceBetweenCoords(p:pos(), v.pos, true)
+                if dst <= 20.0 and not p:GetCrewWarStatus() then
+                    table.insert(closestZones, {zone = v, distance = dst})
                 end
+            end
+            lastUpdateTime = currentTime
+        end
 
-                if dst <= 2.0 then
-                    ShowHelpNotification(v.text, true)
-                    if IsControlJustReleased(0, 38) then
-                        v.actions()
-                    end
+        for _, data in ipairs(closestZones) do
+            local v = data.zone
+            local dst = data.distance
+            if v.haveMarker then
+                DrawMarker(v.markerType, v.pos, 0.0, 0.0, 0.0, v.rotX, v.rotY, v.rotZ, v.markerSize, v.markerSize, v.markerSize, v.markerColor[1], v.markerColor[2], v.markerColor[3], v.markerAlpha, 0, 1, 2, 0, nil, nil, 0)
+            end
+
+            if dst <= 2.0 then
+                ShowHelpNotification(v.text, true)
+                if IsControlJustReleased(0, 38) then
+                    v.actions()
                 end
             end
         end
 
-        if pNear then
-            Wait(1)
-        else
-            Wait(250)
-        end
+        Wait(1)
     end
 end)
