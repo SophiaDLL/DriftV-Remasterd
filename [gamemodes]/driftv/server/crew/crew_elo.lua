e351fe3ec14177b4034ed3bd9fcb83562167be7e
Rubylium <19718604+Rubylium@users.noreply.github.com>
2024-09-05 13:25:38 +0200

    refactor(crew): More readable elo logic. No logic change.

--- [gamemodes]/driftv/server/crew/crew_elo.lua
+++ [gamemodes]/driftv/server/crew/crew_elo.lua
@@ -1,22 +1,33 @@
-local function ExpectedScores (RatA, RatB)
-	return 1 / (1 + 10 ^ ((RatB - RatA) / 400)), 1 / (1 + 10 ^ ((RatA - RatB) / 400))
+local K_FACTOR_LOW = 32
+local K_FACTOR_MEDIUM = 24
+local K_FACTOR_HIGH = 16
+local RATING_THRESHOLD_LOW = 2100
+local RATING_THRESHOLD_MEDIUM = 2400
+local INITIAL_RATING = 1400
+
+local function ExpectedScores(RatA, RatB)
+    local expA = 1 / (1 + 10 ^ ((RatB - RatA) / 400))
+    local expB = 1 / (1 + 10 ^ ((RatA - RatB) / 400))
+    return expA, expB
 end
 
-local function UpdateRating (Rat, Score, ExpScore)
-	local K = 16
-	
-	if (Rat < 2100) then 		K = 32
-	elseif (Rat < 2400) then 	K = 24
-	end
-	
-	return math.floor(Rat + K * (Score - ExpScore))
+local function UpdateRating(Rat, Score, ExpScore)
+    local K = K_FACTOR_HIGH
+
+    if Rat < RATING_THRESHOLD_LOW then
+        K = K_FACTOR_LOW
+    elseif Rat < RATING_THRESHOLD_MEDIUM then
+        K = K_FACTOR_MEDIUM
+    end
+
+    return math.floor(Rat + K * (Score - ExpScore))
 end
 
-local function NewRating ()
-	return 1400
+local function NewRating()
+    return INITIAL_RATING
 end
 
-function CrewEloNewRating(RatA, RatB, ScoreA, ScoreB) -- Does the work. Just input the old ratings and the score. 1 for win, 0 for loss, 0.5 for draw.
-	local ExpA, ExpB = ExpectedScores(RatA, RatB)
-	return UpdateRating(RatA, ScoreA, ExpA), UpdateRating(RatB, ScoreB, ExpB)
+function CrewEloNewRating(RatA, RatB, ScoreA, ScoreB)
+    local ExpA, ExpB = ExpectedScores(RatA, RatB)
+    return UpdateRating(RatA, ScoreA, ExpA), UpdateRating(RatB, ScoreB, ExpB)
 end
