21d418f5cb3656b80b2d1454c49e3878b541c7ec
Rubylium <19718604+Rubylium@users.noreply.github.com>
2024-09-05 13:16:45 +0200

    chore: Add local scope to 'money' variable in commands.lua

--- [gamemodes]/driftv/server/commands.lua
+++ [gamemodes]/driftv/server/commands.lua
@@ -1,7 +1,7 @@
 RegisterCommand("give", function(source, args, rawCommand)
     if source == 0 then
         source = tonumber(args[1])
-        money = tonumber(args[2])
+        local money = tonumber(args[2])
         player[source].money = math.floor(player[source].money + money)
         RefreshPlayerData(source)
         player[source].needSave = true
